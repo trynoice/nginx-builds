84f4dfc8ab1908dda5b7bd5cbd33dcf832564603
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2024-06-30 16:49:36 +0530

    bump everything and remove amd64 builds

--- nginx-build.sh
+++ nginx-build.sh
@@ -10,19 +10,19 @@ set -euxo pipefail
 # Example: ./nginx-build.sh /usr/local/sbin/nginx
 
 # https://nginx.org/en/download.html
-NGINX_VERSION=1.23.3
+NGINX_VERSION=1.27.0
 
 # https://github.com/vision5/ngx_devel_kit/releases
-NGX_DEVEL_KIT_VERSION=0.3.2
+NGX_DEVEL_KIT_VERSION=0.3.3
 
 # https://github.com/openresty/set-misc-nginx-module/tags
 NGX_SET_MISC_MOD_VERSION=0.33
 
 # https://github.com/openresty/headers-more-nginx-module/tags
-NGX_HEADERS_MORE_MOD_VERSION=0.34
+NGX_HEADERS_MORE_MOD_VERSION=0.37
 
 # https://hg.nginx.org/njs/tags
-NGX_NJS_MOD_VERSION=0.7.10
+NGX_NJS_MOD_VERSION=0.8.4
 
 NGINX_SRC_URL=http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 NGX_DEVEL_KIT_SRC_URL=https://github.com/vision5/ngx_devel_kit/archive/refs/tags/v${NGX_DEVEL_KIT_VERSION}.tar.gz
